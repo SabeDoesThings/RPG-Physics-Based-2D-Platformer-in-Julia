e395de0a12f4eab2c526ae60120670e1b040729d
SabeDoesThings <122580233+SabeDoesThings@users.noreply.github.com>
2024-07-27 20:19:55 -0500

    Add files via upload

---
+++ Part 4/src/SDLPlatformer.jl
@@ -0,0 +1,34 @@
+using SimpleDirectMediaLayer
+using SimpleDirectMediaLayer.LibSDL2
+
+include("RenderWindow.jl")
+
+function main()
+    @assert SDL_Init(SDL_INIT_VIDEO) == 0 "error initializing SDL: $(unsafe_string(SDL_GetError()))"
+    @assert IMG_Init(IMG_INIT_PNG) != 0 "error initializing IMG: $(unsafe_string(SDL_GetError()))"
+
+    window = RenderWindow("GAME v1.0", 1280, 720)
+
+    grass_texture = load_texture("res/ground_grass_1.png", window)
+
+    game_running = true
+
+    event = Ref{SDL_Event}()
+
+    while game_running
+        while Bool(SDL_PollEvent(event))
+            if event[].type == SDL_QUIT
+                game_running = false
+            end
+        end
+
+        clear(window)
+        render(grass_texture, window)
+        display(window)
+    end
+
+    clean_up(window)
+    SDL_Quit()
+end
+
+main()
